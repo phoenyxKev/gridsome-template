82e44eddbdc010f4a2f15604d68536e8c862aeb2
kg-networks <kevin.jgnetworks@gmail.com>
2022-10-29 12:21:26 +0200

    first commit

---
+++ src/pages/blogHtml/firstPost.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layout>
+    <h1>Fist post</h1>
+    <img src="//unsplash.it/510" alt="blog image" />
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+  </Layout>
+</template>
+
+<script lang="ts">
+export default {
+  metaInfo: {
+    title: 'First Post'
+  }
+}
+</script>
